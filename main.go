59a4bd15f96f57c0e01d627f3d455233b0a9ca55
unknown <ashiq.buet73@gmail.com>
2025-10-18 22:02:41 +0600

    Type of go variabale

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ func main() {
 	const conferenceTickers = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickers, "tickets and", remainingTickets, "are still available")
-	fmt.Println("Get your tickets here to attend")
+	fmt.Printf("ConfernceTickets is %T, remaining Tickets is %T, ConferenceName is %T\n", conferenceTickers, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickers, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Abid"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
